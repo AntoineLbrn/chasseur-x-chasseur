378eb353b57ac20462aee9dafb5d497ee80a8bd8
Alexandre <alexandre.lepicier@gmail.com>
2022-06-29 01:24:46 +0200

    ajout engimes -> 33

--- src/enigmes.js
+++ src/enigmes.js
@@ -9,30 +9,41 @@ module.exports = [
     [8, {question: 'Quel est le nom de code de Loid Forger dans Spy x Family ?', answers: ["TWILIGHT"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1655929519/Loid_Forger_Colored_Full_Body_x1oqa6.webp", id: 8, name: 'Twilight', description: "Personnage de SxF", rarity: "Commune"}],
     [9, {question: 'Combien y a-t-il de Titans Primordiaux dans L\'Attaque des Titans ?', answers: ["9", "NEUF"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1655927651/Eren-Yeager-PNG-Image_nleieg.png", id: 9, name: 'Eren', description: "Personnage de SNK", rarity: "Rare"}],
     [10, {question: 'Quel animateur japonais est célèbre pour avoir animé les scènes d\'action de Levi dans L\'Attaque des Titans ?', answers: ["IMAIARIFUMI", "ARIFUMIIMAI"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1655921231/Captain-Levi-Ackerman-PNG-Transparent-Image_xbros8.png", id: 10, name: 'Levi', description: "Personnage de SNK", rarity: "Épique"}],
-    
-    [11, {question: 'Comment s\'appelle la décoration culinaire japonaise à base de poisson et avec une spirale rose ?', answers: ["NARUTOMAKI"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656355303/11-20/Naruto-Shippuden-PNG-Free-Download_nkljj7.png", id: 11, name: 'Naruto', description: "Personnage de Naruto", rarity: "Commune"}],
-    [12, {question: 'Comment dit on "cerisier" en japonais ?', answers: ["SAKURA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656355647/11-20/73-732685_sakura-4-naruto-shippuden-sakura-png_kniumq.png", id: 12, name: 'Sakura', description: "Personnage de Naruto", rarity: "Commune"}],
+
+    [11, {question: 'Dans One Piece, comment s\'appellent les fruits du démon qui permettent à son utilisateur de prendre la forme d\'un élément naturel ?', answers: ["LOGIA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656358593/11-20/q7LDgDk_e8juay.png", id: 11, name: 'Law', description: "Personnage de OP", rarity: "Commune"}],
+    [12, {question: 'Quel est le nom de l\'île natale de Luffy, le personnage de One Piece ?', answers: ["DAWN"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656358687/11-20/Luffy-PNG-Photos_xqt30h.png", id: 12, name: 'Luffy', description: "Personnage de OP", rarity: "Commune"}],
     [13, {question: 'Comment s\'appelle le jeu pierre-feuille-ciseaux au Japon ?', answers: ["JANKENPON"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656355748/11-20/Ryuko-Matoi-PNG-HD_gsh0oz.png", id: 13, name: 'Ryuko', description: "Personnage de KLK", rarity: "Rare"}],
     [14, {question: 'Quel est le nom de l\'art du découpage du papier dans la culture japonaise ?', answers: ["KIRIGAMI"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656356118/11-20/c0a6fda4ac637c8c61d9ee568ab6e1c3_sebnim.png", id: 14, name: 'Mako', description: "Personnage de KLK", rarity: "Rare"}],
     [15, {question: 'Quel est le plat traditionel de Noël au Japon ?', answers: ["KFC"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656356422/11-20/a5ed2270b64d13210ba34b4233d3aa78_gsguh3.jpg", id: 15, name: 'Itachi et sa poule', description: "Itachi et sa poule", rarity: "Épique"}],
-    [16, {question: 'Quel artiste japonais a peint ce tableau ? ("La Grande Vague de Kanagawa")', answers: ["HOKUSAI", "KATSUSHIKAHOKUSAI"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656356590/11-20/image_2022-05-12_031619872-819x1024_fyexkf.png", id: 16, name: 'Yaguchi', description: "Personnage de Blue Period", rarity: "Rare"}],
+    [16, {question: 'Quel artiste japonais a peint ce tableau ? ("La Grande Vague de Kanagawa")', answers: ["HOKUSAI", "KATSUSHIKAHOKUSAI", "HOKUSAIKATSUSHIKA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656356590/11-20/image_2022-05-12_031619872-819x1024_fyexkf.png", id: 16, name: 'Yaguchi', description: "Personnage de Blue Period", rarity: "Rare"}],
     [17, {question: 'Comment s\'appelle le mouvement artistique japonais qui consiste à graver des estampes sur du bois ?', answers: ["UKIYO-E", "UKIYOE", "UKIYO"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656357981/11-20/Kuwana.Maki.full.3611735_kyyqya.png", id: 17, name: 'Maki', description: "Personnage de Blue Period", rarity: "Épique"}],
     [18, {question: 'En quelle année est sortie le premier film du studio Ghibli ?', answers: ["1986"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656358343/11-20/My-Neighbor-Totoro-Transparent-Background_cmuxlw.png", id: 18, name: 'Totoro', description: "Personnage du Studio Ghibli", rarity: "Commune"}],
-    [19, {question: 'Dans One Piece, comment s\'appellent les fruits du démon qui permettent à son utilisateur de prendre la forme d\'un élément naturel ?', answers: ["LOGIA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656358593/11-20/q7LDgDk_e8juay.png", id: 19, name: 'Law', description: "Personnage de OP", rarity: "Commune"}],
-    [20, {question: 'Quel est le nom de l\'île natale de Luffy, le personnage de One Piece ?', answers: ["DAWN"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656358687/11-20/Luffy-PNG-Photos_xqt30h.png", id: 20, name: 'Luffy', description: "Personnage de OP", rarity: "Commune"}],
-    
-    [21, {question: 'Who\'s that Pokemon ?', answers: ["MIMIQUI", "MIMIKYU"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369481/21-30/1200px-Mimiqui__Forme_D%C3%A9guis%C3%A9e_-SL_pakx0h.png", id: 21, name: 'Mimiqui', description: "Pokemon", rarity: "Épique"}],
-    [22, {question: 'Who\'s that Pokemon ?', answers: ["GERMIGNON", "CHIKORITA"],  url: "", id: 22, name: 'Germignon', description: "Pokemon", rarity: "Commune"}],
-    [23, {question: 'Who\'s that Pokemon ?', answers: ["FOUINAR", "FURRET"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369482/21-30/1200px-Fouinar-HGSS_rqhga2.png", id: 23, name: 'Fouinar', description: "Pokemon", rarity: "Rare"}],
-    [24, {question: 'Who\'s that Pokemon ?', answers: ["SALAMÈCHE", "SALAMECHE", "CHARMANDER"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369481/21-30/Salam%C3%A8che-RFVF_djpunz.png", id: 24, name: 'Salamèche', description: "Pokemon", rarity: "Commune"}],
-    [25, {question: '', answers: [""],  url: "", id: 25, name: '', description: "Personnage de", rarity: ""}],
-    [26, {question: '', answers: [""],  url: "", id: 26, name: '', description: "Personnage de", rarity: ""}],
-    [27, {question: '', answers: [""],  url: "", id: 27, name: '', description: "Personnage de", rarity: ""}],
-    [28, {question: '', answers: [""],  url: "", id: 28, name: '', description: "Personnage de", rarity: ""}],
-    [29, {question: '', answers: [""],  url: "", id: 29, name: '', description: "Personnage de", rarity: ""}],
-    [30, {question: '', answers: [""],  url: "", id: 30, name: '', description: "Personnage de", rarity: ""}], // 3 rare, 1 epique
+    [19, {question: 'Comment s\'appelle la décoration culinaire japonaise à base de poisson et avec une spirale rose ?', answers: ["NARUTOMAKI"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656355303/11-20/Naruto-Shippuden-PNG-Free-Download_nkljj7.png", id: 19, name: 'Naruto', description: "Personnage de Naruto", rarity: "Commune"}],
+    [20, {question: 'Comment dit on "cerisier" en japonais ?', answers: ["SAKURA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656355647/11-20/73-732685_sakura-4-naruto-shippuden-sakura-png_kniumq.png", id: 20, name: 'Sakura', description: "Personnage de Naruto", rarity: "Commune"}],
 
-    [31, {question: '', answers: [""],  url: "", id: 31, name: '', description: "Personnage de", rarity: ""}]
+    [21, {question: 'Who\'s that Pokemon ?', answers: ["MIMIQUI","MIMIKYU"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369481/21-30/1200px-Mimiqui__Forme_D%C3%A9guis%C3%A9e_-SL_pakx0h.png", id: 21, name: 'Mimiqui', description: "Pokemon", rarity: "Épique"}],
+    [22, {question: 'Who\'s that Pokemon ?', answers: ["GERMIGNON","CHIKORITA"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369481/21-30/Germignon-HGSS_v5xtdw.png", id: 22, name: 'Germignon', description: "Pokemon", rarity: "Commune"}],
+    [23, {question: 'Who\'s that Pokemon ?', answers: ["FOUINAR","FURRET"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369482/21-30/1200px-Fouinar-HGSS_rqhga2.png", id: 23, name: 'Fouinar', description: "Pokemon", rarity: "Rare"}],
+    [24, {question: 'Who\'s that Pokemon ?', answers: ["SALAMÈCHE","SALAMECHE","CHARMANDER"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656369481/21-30/Salam%C3%A8che-RFVF_djpunz.png", id: 24, name: 'Salamèche', description: "Pokemon", rarity: "Commune"}],
+    [25, {question: 'Qui est ce personnage ?', answers: ["EDWARD","EDWARDELRIC","ELRICEDWARD"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/NicePng_edward-elric-png_1698428_dr9ufn.png", id: 25, name: 'Edward', description: "Personnage de FMAB", rarity: "Commune"}],
+    [26, {question: 'Qui est ce personnage ?', answers: ["SENKU","SENKUISHIGAMI","ISHIGAMISENKU"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/Senku_Ishigami_Anime_corbhg.webp", id: 26, name: 'Senku', description: "Personnage de Dr.Stone", rarity: "Rare"}],
+    [27, {question: 'Qui est ce personnage ?', answers: ["L"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/Death_Note_L_v342wu.webp", id: 27, name: 'L', description: "Personnage de Death Note", rarity: "Commune"}],
+    [28, {question: 'Qui est ce personnage ?', answers: ["MEGUMIN"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/toppng.com-megumin-5pb-kono-subarashii-sekai-ni-shukufuku-wo-kono-yokubukai-799x990_jkp9o2.png", id: 28, name: 'Megumin', description: "Personnage de Konosuba", rarity: "Rare"}],
+    [29, {question: 'Qui est ce personnage ?', answers: ["KYOJURORENGOKU","RENGOKUKYOJURO","RENGOKU","KYOJURO"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/Rengoku_Profile_hgrgmw.webp", id: 29, name: 'Rengoku', description: "Personnage de Demon Slayer", rarity: "Épique"}],
+    [30, {question: 'Qui est ce personnage ?', answers: ["HINATA","SHOYOHINATA","HINATASHOYO"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656454933/21-30/dlf.pt-hinata-png-4615651_bilmfp.png", id: 30, name: 'Hinata', description: "Personnage de Haikyuu", rarity: "Commune"}],
+
+    [31, {question: 'Décodez ce message :', answers: ["VOEUX"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656456853/31-40/Emma_Anime_ctwqb1.webp", id: 31, name: 'Emma', description: "Personnage de Promised Neverland", rarity: "Épique"}],
+    [32, {question: 'Décodez ce message :', answers: ["JAMAIS"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656456853/31-40/Norman_Anime_r2z2dw.webp", id: 32, name: 'Norman', description: "Personnage de Promised Neverland", rarity: "Commune"}],
+    [33, {question: 'Décodez ce message :', answers: ["TERRE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656456854/31-40/ray-1_zh1khr.png", id: 33, name: 'Ray', description: "Personnage de Promised Neverland", rarity: "Commune"}],
+    [34, {question: 'Décodez ce message :', answers: [""],  url: "", id: 34, name: '', description: "Personnage de", rarity: ""}],
+    [35, {question: 'Décodez ce message :', answers: [""],  url: "", id: 35, name: '', description: "Personnage de", rarity: ""}],
+    [36, {question: 'Décodez ce message :', answers: [""],  url: "", id: 36, name: '', description: "Personnage de", rarity: ""}],
+    [37, {question: 'Décodez ce message :', answers: [""],  url: "", id: 37, name: '', description: "Personnage de", rarity: ""}],
+    [38, {question: 'Décodez ce message :', answers: [""],  url: "", id: 38, name: '', description: "Personnage de", rarity: ""}],
+    [39, {question: 'Décodez ce message :', answers: [""],  url: "", id: 39, name: '', description: "Personnage de", rarity: ""}],
+    [40, {question: '', answers: [""],  url: "", id: 40, name: '', description: "Personnage de", rarity: ""}], // 3 rare, 1 epique, manga en plus ?
+    
+    [41, {question: '', answers: [""],  url: "", id: 41, name: '', description: "Personnage de", rarity: ""}],
 ]   
 
 // 50 énigmes
@@ -41,10 +52,7 @@ module.exports = [
 // 1 Intro
 // 2-10 Culture Manga
 // 11-20 Culture Jap
-
 // 21-30 Silhouettes
-// 31-40 Culture Enigmes (morse = PNL x 3 voeux jamais terre, lettre miroir x3, code césar x3 ) 9
-// 41-50 Anim (tournoi ninja, cache cache, PFC ++, morpion ++)
 
-// Manga en plus (FMAB ed, dr stone, L, megumin, demon slayer rangyoku, haikyuu (https://www.youtube.com/watch?v=Q5JMw1UaofY))
-// Images sur cartes pokemon et peintures hokusai
+// 31-40 Culture Enigmes (lettre miroir x3, code césar x3 ) 9
+// 41-50 Anim (tournoi ninja, cache cache, PFC ++, morpion ++)
