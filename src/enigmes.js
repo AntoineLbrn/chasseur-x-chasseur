dd8008a0c956acda8641128d42bf9b1a2d8c9ae5
Alexandre <alexandre.lepicier@gmail.com>
2022-06-29 19:05:49 +0200

    modif alex

--- src/enigmes.js
+++ src/enigmes.js
@@ -38,9 +38,9 @@ module.exports = [
     [34, {question: 'Décodez ce code césar de 5 :', answers: ["LUNE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656514685/31-40/Capture_mprldh.png", id: 34, name: 'Kaguya', description: "Personnage de Kaguya Sama: Love is war", rarity: "Commune"}],
     [35, {question: 'Décodez ce code césar de 5 :', answers: ["ARGENT"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656514740/31-40/shirogane_miyuki_17945_vydkmj.jpg", id: 35, name: 'Shirogane', description: "Personnage de Kaguya Sama: Love is war", rarity: "Commune"}],
     [36, {question: 'Décodez ce code césar de 5 :', answers: ["POULET"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656514782/31-40/EvkxJWwXIAEbbZb_v64h0s.jpg", id: 36, name: 'Fujiwara', description: "Personnage de Kaguya Sama: Love is war", rarity: "Rare"}],
-    [37, {question: 'Décodez ce message :', answers: ["LETTRE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656515544/31-40/conan_fg3b2w.png", id: 37, name: 'Conan', description: "Personnage de Detective Conan", rarity: "Commune"}],
-    [38, {question: 'Décodez ce message :', answers: ["VOYAGE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656516726/31-40/Hinazuki.Kayo.full.1977538_s7iykd.jpg", id: 38, name: 'Kayo', description: "Personnage de Erased", rarity: "Rare"}],
-    [39, {question: 'Décodez ce message :', answers: ["INDICE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656517109/31-40/b4aceb004b1e02534a924b393260fad4ab57fd65v2_00_alq3z7.jpg", id: 39, name: 'Kaori', description: "Personnage de Your Lie in April", rarity: "Épique"}],
+    [37, {question: 'Décodez ce message :', answers: ["LETTRE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656515544/31-40/conan_fg3b2w.png", id: 37, name: 'Conan', description: "Personnage de Detective Conan", rarity: "Épique"}],
+    [38, {question: 'Décodez ce message :', answers: ["VOYAGE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656520943/31-40/Ran_Mouri__28Profil_29_qkonzy.webp", id: 38, name: 'Ran', description: "Personnage de Detective Conan", rarity: "Rare"}],
+    [39, {question: 'Décodez ce message :', answers: ["INDICE"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656517109/31-40/b4aceb004b1e02534a924b393260fad4ab57fd65v2_00_alq3z7.jpg", id: 39, name: 'Kaori', description: "Personnage de Your Lie in April", rarity: "Rare"}],
     [40, {question: 'Décodez ce message :', answers: ["PIANO"],  url: "https://res.cloudinary.com/dbhr3yfi5/image/upload/v1656517674/31-40/Kosei_Arima_Infobox_wmipbz.webp", id: 40, name: 'Kosei', description: "Personnage de Your Lie in April", rarity: "Commune"}],
     
     [41, {question: '', answers: [""],  url: "", id: 41, name: '', description: "Personnage de", rarity: ""}],
@@ -53,6 +53,6 @@ module.exports = [
 // 2-10 Culture Manga
 // 11-20 Culture Jap
 // 21-30 Silhouettes
+// 31-40 Enigmes
 
-// 31-40 Culture Enigmes (lettre miroir x3, code césar x3 ) 9
 // 41-50 Anim (tournoi ninja, cache cache, PFC ++, morpion ++)
